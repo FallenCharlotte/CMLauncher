ad1bf7bada4a884d14dc4d71af3252dd3ed689df
Thomas Cheyney <top_cat@thomasc.co.uk>
2020-11-06 17:24:49 +0000

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,262 @@
+﻿using SharpCompress.Readers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace CM_Launcher
+{
+    public partial class Form1 : Form, IProgress<float>
+    {
+        private readonly SynchronizationContext synchronizationContext;
+        private string VersionFilename;
+
+        public Form1()
+        {
+            InitializeComponent();
+            synchronizationContext = SynchronizationContext.Current;
+            Hide();
+
+            new Main(this);
+        }
+
+        public void Show(string versionFilename, int current, int desired)
+        {
+            Show();
+            VersionFilename = versionFilename;
+
+            new Thread(() =>
+            {
+                DoUpdate(current, desired);
+            }).Start();
+        }
+
+        private void UpdateLabel(string text)
+        {
+            synchronizationContext.Post(new SendOrPostCallback(o =>
+            {
+                label1.Text = $"{o}";
+            }), text);
+        }
+
+        private async Task<int> UpdateUsingZip(int version)
+        {
+            UpdateLabel("Downloading update...");
+            string downloadUrl = Main.USE_CDN ? $"{Main.CDN_URL}/{version}/Win64.zip" : $"https://jenkins.kirkstall.top-cat.me/job/ChroMapper/{version}/artifact/ChroMapper-Win64.zip";
+
+            using (var tmp = new TempFile())
+            {
+                using (var client = new HttpClient())
+                {
+                    client.Timeout = TimeSpan.FromMinutes(5);
+
+                    using (var file = new FileStream(tmp.Path, FileMode.Create, FileAccess.Write, FileShare.None))
+                    {
+                        await client.DownloadAsync(downloadUrl, file, this);
+                    }
+                }
+
+                ExtractZip(tmp.Path);
+
+                SetVersion(version);
+
+                return version;
+            }
+        }
+
+        private async Task<int> UpdateUsingPatch(int source, int dest)
+        {
+            UpdateLabel($"Downloading patch for {dest}");
+            string downloadUrl = $"{Main.CDN_URL}/{dest}/{source}.patch";
+
+            using (var tmp = new TempFile())
+            {
+                using (var client = new HttpClient())
+                {
+                    client.Timeout = TimeSpan.FromMinutes(5);
+
+                    using (var file = new FileStream(tmp.Path, FileMode.Create, FileAccess.Write, FileShare.None))
+                    {
+                        await client.DownloadAsync(downloadUrl, file, this);
+                    }
+                }
+
+                ApplyPatch(tmp.Path);
+                SetVersion(dest);
+
+                return dest;
+            }
+        }
+
+        private void ApplyPatch(string filename)
+        {
+            Report(0);
+            using (Stream stream = File.OpenRead(filename))
+            {
+                var reader = ReaderFactory.Open(stream);
+                while (reader.MoveToNextEntry())
+                {
+                    if (!reader.Entry.IsDirectory)
+                    {
+                        UpdateLabel($"Patching {reader.Entry.Key.Replace("xdelta/", "")}");
+                        Report(((float) stream.Position) / stream.Length);
+
+                        using (MemoryStream memStream = new MemoryStream(100))
+                        {
+                            reader.WriteEntryTo(memStream);
+
+                            string patchFilename = reader.Entry.Key.Replace("xdelta", "chromapper");
+                            byte[] oldFile = File.ReadAllBytes(patchFilename);
+                            byte[] newFile = xdelta3.ApplyPatch(memStream.ToArray(), oldFile);
+                            File.WriteAllBytes(patchFilename, newFile);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void ExtractZip(string filename)
+        {
+            UpdateLabel("Extracting zip");
+            Report(0);
+
+            using (var archive = ZipFile.Open(filename, ZipArchiveMode.Read))
+            {
+                string destinationDirectoryFullPath = Main.LauncherFolder;
+                float total = archive.Entries.Count();
+                int current = 0;
+
+                foreach (ZipArchiveEntry file in archive.Entries)
+                {
+                    string completeFileName = Path.GetFullPath(Path.Combine(destinationDirectoryFullPath, file.FullName));
+
+                    UpdateLabel($"Extracting {file.FullName.Replace("chromapper/", "")}");
+                    Report(current++ / total);
+
+                    if (!completeFileName.StartsWith(destinationDirectoryFullPath, StringComparison.OrdinalIgnoreCase))
+                    {
+                        throw new IOException("Trying to extract file outside of destination directory");
+                    }
+
+                    Directory.CreateDirectory(Path.GetDirectoryName(completeFileName));
+                    if (file.Name == "") // Assuming Empty for Directory
+                    {
+                        continue;
+                    }
+
+                    file.ExtractToFile(completeFileName, true);
+                }
+            }
+        }
+
+        private void SetVersion(int version)
+        {
+            File.WriteAllText(VersionFilename, $"{version}");
+        }
+
+        private async Task<List<int>> FindPath(int current, int desired)
+        {
+            var regex = new Regex(@"[0-9]+/([0-9]+).patch");
+            var patches = new List<int>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync($"{Main.CDN_URL}?prefix={desired}/"))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        var stream = await content.ReadAsStreamAsync();
+                        XmlReader xReader = XmlReader.Create(stream);
+
+                        while (xReader.ReadToFollowing("Contents"))
+                        {
+                            xReader.ReadToFollowing("Key");
+                            var str = xReader.ReadElementContentAsString();
+                            var matches = regex.Matches(str);
+                            
+                            int possible = int.Parse(matches[0].Groups[1].Value);
+                            patches.Add(possible);
+                        }
+                    }
+                }
+            }
+            
+            if (patches.Contains(current))
+            {
+                // We can patch from here!
+                var ret = new List<int>
+                {
+                    desired
+                };
+                return ret;
+            }
+
+            try
+            {
+                var oldest = patches.Where(a => a > current).Min();
+                var ret2 = await FindPath(current, oldest);
+
+                if (ret2 != null)
+                    ret2.Add(desired);
+
+                return ret2;
+            }
+            catch (InvalidOperationException)
+            {
+                return null;
+            }
+        }
+
+        private async void DoUpdate(int current, int desired)
+        {
+            int stable = await Main.GetLatestBuildNumber(ReleaseChannel.Stable);
+
+            if (current == 0 || current > desired)
+            {
+                current = await UpdateUsingZip(stable);
+            }
+
+            if (current < desired)
+            {
+                var patches = await FindPath(current, desired);
+
+                if (patches == null)
+                {
+                    // We can't patch to the desired version
+                    // If we're not on stable go there
+                    if (current != stable)
+                    {
+                        await UpdateUsingZip(stable);
+                    }
+
+                    // Abandon ship!
+                    // Hopefully someone creates an update path for us next time around
+                    Main.Exit();
+                    return;
+                }
+
+                foreach (int patch in patches)
+                {
+                    current = await UpdateUsingPatch(current, patch);
+                }
+            }
+
+            Main.Exit();
+        }
+
+        public void Report(float value)
+        {
+            synchronizationContext.Post(new SendOrPostCallback(o =>
+            {
+                progressBar1.Value = (int) o;
+            }), (int)(value * 1000));
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,109 @@
+﻿using SimpleJSON;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CM_Launcher
+{
+    class Main
+    {
+        public static readonly bool USE_CDN = true;
+        public static string CDN_URL = "https://cm.topc.at";
+        public static readonly string LauncherFolder = AppDomain.CurrentDomain.BaseDirectory;
+
+        private readonly string VersionFilename;
+        private readonly Form1 form;
+
+        private readonly ReleaseChannel useChannel = ReleaseChannel.Stable;
+
+        private int GetVersion()
+        {
+            if (!File.Exists(VersionFilename)) return 0;
+
+            return int.Parse(File.ReadAllText(VersionFilename));
+        }
+
+        string GetKnownFolderPath(Guid knownFolderId)
+        {
+            IntPtr pszPath = IntPtr.Zero;
+            try
+            {
+                int hr = SHGetKnownFolderPath(knownFolderId, 0, IntPtr.Zero, out pszPath);
+                if (hr >= 0)
+                    return Marshal.PtrToStringAuto(pszPath);
+                throw Marshal.GetExceptionForHR(hr);
+            }
+            finally
+            {
+                if (pszPath != IntPtr.Zero)
+                    Marshal.FreeCoTaskMem(pszPath);
+            }
+        }
+
+        [DllImport("shell32.dll")]
+        static extern int SHGetKnownFolderPath([MarshalAs(UnmanagedType.LPStruct)] Guid rfid, uint dwFlags, IntPtr hToken, out IntPtr pszPath);
+
+        public Main(Form1 form)
+        {
+            this.form = form;
+            VersionFilename = Path.Combine(LauncherFolder, "cm-version");
+
+            Guid localLowId = new Guid("A520A1A4-1780-4FF6-BD18-167343C5AF16");
+            string cmSettingsPath = Path.Combine(GetKnownFolderPath(localLowId), "BinaryElement", "ChroMapper", "ChroMapperSettings.json");
+
+            using (StreamReader reader = new StreamReader(cmSettingsPath))
+            {
+                JSONNode mainNode = JSON.Parse(reader.ReadToEnd());
+                useChannel = mainNode["ReleaseChannel"].Value == "0" ? ReleaseChannel.Stable : ReleaseChannel.Dev;
+            }
+
+            DoUpdate();
+        }
+
+        public static async Task<int> GetLatestBuildNumber(ReleaseChannel channelEnum)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string channel = channelEnum == ReleaseChannel.Stable ? "stable" : "dev";
+
+                using (HttpResponseMessage response = await client.GetAsync($"{CDN_URL}/{channel}"))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        return int.Parse(await content.ReadAsStringAsync());
+                    }
+                }
+            }
+        }
+
+        private async void DoUpdate()
+        {
+            int current = GetVersion();
+            int desired = await GetLatestBuildNumber(useChannel);
+
+            if (current == 0 || current < desired || (current > desired && useChannel == ReleaseChannel.Stable))
+            {
+                form.Show(VersionFilename, current, desired);
+                return;
+            }
+
+            Exit();
+        }
+
+        public static void Exit()
+        {
+            var startInfo = new ProcessStartInfo("ChroMapper.exe")
+            {
+                WorkingDirectory = Path.Combine(LauncherFolder, "chromapper")
+            };
+
+            Process.Start(startInfo);
+
+            Application.Exit();
+        }
+    }
+}
