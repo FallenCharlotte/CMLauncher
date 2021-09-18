019dd7bc0f47416c2337cf309eecaaeecba07a78
Thomas Cheyney <top_cat@thomasc.co.uk>
2021-09-18 20:55:43 +0100

    WIP

---
+++ Windows5/Form1.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace Windows5
+{
+    public class Form1 : Window
+    {
+        public Form1()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public void button_Click(object sender, RoutedEventArgs e)
+        {
+            // Change button text when button is clicked.
+            var button = (Button)sender;
+            button.Content = "Hello, Avalonia!";
+        }
+    }
+}
