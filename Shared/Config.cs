5c474c480a347da0fa870578bee35d4b1ebc1ec4
Thomas Cheyney <top_cat@thomasc.co.uk>
2022-10-07 15:36:56 +0100

    Bump version

--- Shared/Config.cs
+++ Shared/Config.cs
@@ -1,6 +1,6 @@
 public static class Config
 {
-    public const int APP_VERSION = 11;
+    public const int APP_VERSION = 12;
     public const int PATCH_SKIP_LIMIT = 12;
     public const string CDN_URL = "https://cm.topc.at";
 }
